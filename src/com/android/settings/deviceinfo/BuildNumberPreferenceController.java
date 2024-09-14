Franxx-OS/packages_apps_Settings

Settings: Append custom version on build display id
Change-Id: I831800ae0a769513b6b0c008ba492cd8334b3b65
Signed-off-by: Karan Parashar <whyredfire@gmail.com>

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -79,7 +79,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String customVersion = VersionUtils.getcustomVersion();
+        if (!customVersion.equals("")){
+            sb.append("\n");
+            sb.append(customVersion);
+        }
+        return sb.toString();
     }
 
     @Override

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getcustomVersion(){
+        return SystemProperties.get("ro.custom.version","");
+    }
+}

src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
@@ -21,6 +21,7 @@ import android.os.Build;
 import android.text.BidiFormatter;
 
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.VersionUtils;
 
 public class SimpleBuildNumberPreferenceController extends BasePreferenceController {
 
@@ -36,6 +37,13 @@ public class SimpleBuildNumberPreferenceController extends BasePreferenceControl
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String customVersion = VersionUtils.getcustomVersion();
+        if (!customVersion.equals("")){
+            sb.append("\n");
+            sb.append(customVersion);
+        }
+        return sb.toString();
     }
 }

israfilbd/android_packages_apps_Settings-1

src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -80,7 +80,14 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String PixelageVersion = VersionUtils.getPixelageVersion();
+        if (!PixelageVersion.equals("")){
+            sb.append("\n");
+            sb.append(PixelageVersion);
+        }
+        return sb.toString();
     }
 
     @Override

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -1,0 +1,10 @@
+
+package com.android.settings.deviceinfo;
+
+import android.os.SystemProperties;
+
+public class VersionUtils {
+    public static String getPixelageVersion(){
+        return SystemProperties.get("ro.pixelage.version","");
+    }
+}

src/com/android/settings/deviceinfo/firmwareversion/SimpleBuildNumberPreferenceController.java
@@ -21,6 +21,7 @@ import android.os.Build;
 import android.text.BidiFormatter;
 
 import com.android.settings.core.BasePreferenceController;
+import com.android.settings.deviceinfo.VersionUtils;
 
 public class SimpleBuildNumberPreferenceController extends BasePreferenceController {
 
@@ -36,6 +37,13 @@ public class SimpleBuildNumberPreferenceController extends BasePreferenceControl
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        StringBuilder sb = new StringBuilder();
+        sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
+        String PixelageVersion = VersionUtils.getPixelageVersion();
+        if (!PixelageVersion.equals("")){
+            sb.append("\n");
+            sb.append(PixelageVersion);
+        }
+        return sb.toString();
     }
 }